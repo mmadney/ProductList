a137bbd7a06064b50779ca06bfb30d726aa3bbd6
mmadney <mohamedmadney1@gmail.com>
2022-06-22 13:35:45 +0200

    Initial Commit

---
+++ listings app/listings_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  listings_appApp.swift
+//  listings app
+//
+//  Created by Madney on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct listings_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
